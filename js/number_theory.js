e4ada7a7df61b88b09d895dfced9f3ee7d15e15c
xuanyan <superacidxy@gmail.com>
2014-06-25 18:47:03 +0800

    O(sqrt(n)) for arithmetic functions

--- js/number_theory.js
+++ js/number_theory.js
@@ -1,30 +1,47 @@
-var coprime = function(x, y) {
-	var t;
-	if (x > y) {
-		t = x; x = y; y = t;
-	}
-	while (x >= 2) {
-		t = y % x; y = x; x = t;
-	}
-	return x == 1;
-}
-
 var phi = function(n) {
-	var x = 0;
-	for (var i = 1; i <= n; ++i) {
-		if (coprime(i, n)) {
-			++x;
+	var x = n;
+	if (n % 2 == 0) {
+		x /= 2;
+		do {
+			n /= 2;
+		} while (n % 2 == 0);
+	}
+	for (var p = 3; p * p <= n; p += 2) {
+		if (n % p == 0) {
+			x = x / p * (p - 1);
+			do {
+				n /= p;
+			} while (n % p == 0);
 		}
+	}
+	if (n > 1) {
+		x = x / n * (n - 1);
 	}
 	return x;
 };
 
 var sigma = function(n) {
-	var sum = 0;
-	for (var i = 1; i <= n; ++i) {
-		if (n % i == 0) {
-			sum += i;
+	var sum = 1;
+	var t = 1;
+	if (n % 2 == 0) {
+		do {
+			n /= 2;
+			t = t * 2 + 1;
+		} while (n % 2 == 0);
+		sum *= t;
+	}
+	for (var p = 3; p * p <= n; p += 2) {
+		t = 1;
+		if (n % p == 0) {
+			do {
+				n /= p;
+				t = t * p + 1;
+			} while (n % p == 0);
+			sum *= t;
 		}
 	}
+	if (n > 1) {
+		sum *= n + 1;
+	}
 	return sum;
-}
+};
